d20d914ed3e561ed13a94ba155ce19d64697c494
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2011-11-13 20:56:43 +1300

    setNegativeCommentsVisible(true/false), only use cn-line "display)

--- extreme-doc.js
+++ extreme-doc.js
@@ -1,18 +1,16 @@
 $(document).ready(function() {
     addHideShowCheckbox();
-    showOrHideNegativeComments(false);
+    setNegativeCommentsVisible(false);
   });
 
-function showOrHideNegativeComments(show) {
-  var visibilityProperty =  show ? "visible" : "hidden";
-  var displayProperty = show ? "block" : "none";
-  $(".cn").css("visibility", visibilityProperty);
+function setNegativeCommentsVisible(visible) {
+  var displayProperty = visible ? "block" : "none";
   $(".cn-line").css("display", displayProperty);
 }
 
 function addHideShowCheckbox() {
   $("body").prepend("<div><span class = 'showNegatives'>Show negative comments<input id = 'showNegatives' type = 'checkbox'></span></div>");
   $("#showNegatives").change(function(event) {
-      showOrHideNegativeComments(event.target.checked);
+      setNegativeCommentsVisible(event.target.checked);
     });
 }
